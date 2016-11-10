b3f19ba005e6e0097ee14370abcd8502bf181200
dytk2134 <dytk2134@gmail.com>
2016-11-10 11:33:13 +0800

    Add fasta_diff.py and update_gff.py

---
+++ bin/fasta_diff.py
@@ -0,0 +1,284 @@
+#! /usr/local/bin/python2.7
+# Copyright (C) 2014  Han Lin <hotdogee [at] gmail [dot] com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+"""
+Compare two very similar FASTA files and output coordinate mappings
+"""
+
+__version__ = '1.1'
+
+try:
+   import cPickle as pickle
+except:
+   import pickle
+from os.path import isfile
+from collections import OrderedDict
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
+
+def fasta_file_to_dict(fasta_file, id=True, header=False, seq=False):
+    """Returns a dict from a fasta file and the number of sequences as the second return value.
+    fasta_file can be a string path or a file object.
+    The key of fasta_dict can be set using the keyword arguments and
+    results in a combination of id, header, sequence, in that order. joined with '||'. (default: id)
+    Duplicate keys are checked and a warning is logged if found.
+    The value of fasta_dict is a python dict with 3 keys: header, id and seq
+    """
+    fasta_file_f = fasta_file
+    if isinstance(fasta_file, str):
+        fasta_file_f = open(fasta_file, 'rb')
+
+    fasta_dict = dict()
+    flags = OrderedDict([('id', id), ('header', header), ('seq', seq)])
+    entry = dict([('id', ''), ('header', ''), ('seq', '')])
+    count = 0
+    line_num = 0
+  
+    for line in fasta_file_f:
+        line = line.strip()
+        if line[0] == '>':
+            count += 1
+            key = '||'.join([entry[i] for i in flags if flags[i]])
+            if key: # key != ''
+                if key in fasta_dict: # check for duplicate key
+                    logging.warning('%s : Line %d : Duplicate %s [%s] : ID = [%s].', fasta_file_f.name, line_num, '||'.join([i for i in flags if flags[i]]), key[:25] + (key[25:] and '..'), entry['id'])
+                fasta_dict[key] = entry
+                entry = dict()
+            entry['header'] = line
+            entry['id'] = line.split()[0][1:]
+            entry['seq'] = ''
+        else:
+            entry['seq'] += line.upper()
+        line_num += 1
+
+    if isinstance(fasta_file, str):
+        fasta_file_f.close()
+
+    key = '||'.join([entry[i] for i in flags if flags[i]])
+    if key: # key != ''
+        if key in fasta_dict:
+            logging.warning('%s : Line %d : Duplicate %s [%s] : ID = [%s].', fasta_file_f.name, line_num, '||'.join([i for i in flags if flags[i]]), key[:25] + (key[25:] and '..'), entry['id'])
+        fasta_dict[key] = entry
+      
+    return fasta_dict, count
+
+def fasta_dict_to_file(fasta_dict, fasta_file):
+    """Write fasta_dict to a fasta_file
+    fasta_file can be a string path or a file object
+    The key of fasta_dict doesn't matter
+    The value of fasta_dict is a python dict with 3 keys: header, id and seq
+    """
+    if isinstance(fasta_file, str):
+        fasta_file = open(fasta_file, 'wb')
+  
+    for key in fasta_dict:
+        fasta_file.write(fasta_dict[key]['header'] + '\n' + fasta_dict[key]['seq'] + '\n')
+
+    fasta_file.close()
+
+def query_yes_no(question, default='yes'):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    'question' is a string that is presented to the user.
+    'default' is the presumed answer if the user just hits <Enter>.
+        It must be 'yes' (the default), 'no' or None (meaning
+        an answer is required of the user).
+
+    The 'answer' return value is one of 'yes' or 'no'.
+    """
+    valid = {'yes': True, 'y': True, 'ye': True,
+             'no': False, 'n': False}
+    if default is None:
+        prompt = ' [y/n] '
+    elif default == 'yes':
+        prompt = ' [Y/n] '
+    elif default == 'no':
+        prompt = ' [y/N] '
+    else:
+        raise ValueError('invalid default answer: "%s"' % default)
+
+    while True:
+        sys.stderr.write(question + prompt)
+        choice = raw_input().strip().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stderr.write('Please respond with "y" or "n".\n')
+            
+def fasta_diff(old_fasta_file, new_fasta_file, debug=True, header_check=False):
+    """
+    Compares two very similar FASTA files and outputs coordinate mappings using a multi stage algorithm:
+    Stage 1: Find 100% matches
+    Stage 2: Find 100% substrings, where the full length of a new sequence can be found as a substring of a old sequence
+    Stage 3: Find cases where part of the sequence was converted into Ns
+    Originally created to compare the NCBI version to the original reference of Diaphorina citri
+    :param old_fasta_file: a string path
+    :param new_fasta_file: a string path
+    :param debug: If True, partial results are saved in a *_stage_i_pickle file after each stage, unmatched sequences after each stage are saved in a *_stage_i_unmatched file for both FASTA files
+    :return: a list of [old_id, old_start, old_end, new_id, new_start, new_end], 0-based coordinate system
+    """
+    alignment_list = []
+    old_fasta_dict, new_fasta_dict = None, None
+
+    def match_identical_sequence():
+        # find 100% matches
+        #[old_id, old_start, old_end, new_id, new_start, new_end]
+        old_seqs = old_fasta_dict.keys()
+        for seq in old_seqs:
+            if seq in new_fasta_dict:
+                alignment = [old_fasta_dict[seq]['id'], 0, len(seq), new_fasta_dict[seq]['id'], 0, len(seq)]
+                if header_check and not old_fasta_dict[seq]['id'].split('.')[0] in new_fasta_dict[seq]['header']:
+                    logging.warning('Failed header check (match_identical_sequence): %s -> %s', old_fasta_dict[seq]['id'], new_fasta_dict[seq]['id'])
+                alignment_list.append(alignment)
+                del old_fasta_dict[seq]
+                del new_fasta_dict[seq]
+
+    def match_truncated_sequence():
+        # Find 100% substrings, where the full length of a new sequence can be found as a substring of a old sequence
+        old_seqs = old_fasta_dict.keys()
+        new_seqs = new_fasta_dict.keys()
+        for new_seq in new_seqs:
+            matches = filter(lambda old_seq: new_seq in old_seq, old_seqs)
+            if len(matches) == 1:
+                start = matches[0].find(new_seq)
+                alignment = [old_fasta_dict[matches[0]]['id'], start, start + len(new_seq), new_fasta_dict[new_seq]['id'], 0, len(new_seq)]
+                if header_check and not old_fasta_dict[matches[0]]['id'].split('.')[0] in new_fasta_dict[new_seq]['header']:
+                    logging.warning('Failed header check (match_truncated_sequence): %s -> %s', old_fasta_dict[matches[0]]['id'], new_fasta_dict[new_seq]['id'])
+                alignment_list.append(alignment)
+                del old_fasta_dict[matches[0]]
+                del new_fasta_dict[new_seq]
+            elif len(matches) > 1:
+                logging.warning('Failed one to one mapping: %s has %d matches: %s\n' % (new_fasta_dict[new_seq]['id'], len(matches), ','.join([old_fasta_dict[x]['id'].split('.')[0] for x in matches])))
+
+    def match_split_subsequence():
+        # Find cases where part of the sequence was converted into Ns
+        # algorithm:
+        # 1. split new seq by Ns into multiple substrings
+        # 2. find an old seq with all substrings and also in order
+        # 3. if found, write an alignment line for every substring
+        old_seqs = old_fasta_dict.keys()
+        new_seqs = new_fasta_dict.keys()
+        for new_seq in new_seqs:
+            segments = new_seq.replace('N', ' ').split()
+            matches = []
+            for old_seq in old_seqs:
+                start_original = 0
+                start_new = 0
+                seg_matches = []
+                for segment in segments:
+                    pos = old_seq.find(segment, start_original)
+                    if pos == -1:
+                        break
+                    else:
+                        start_original = pos + len(segment)
+                        pos_new = new_seq.find(segment, start_new)
+                        start_new = pos_new + len(segment)
+                        seg_matches.append([old_fasta_dict[old_seq]['id'], pos, pos + len(segment), new_fasta_dict[new_seq]['id'], pos_new, pos_new + len(segment)])
+                if len(segments) == len(seg_matches):
+                    matches.append((seg_matches, old_seq))
+            if len(matches) == 1:
+                if header_check and not matches[0][0][0][0].split('.')[0] in new_fasta_dict[new_seq]['header']:
+                    logging.warning('Failed header check (match_split_subsequence): %s -> %s', matches[0][0][0][0], matches[0][0][0][3])
+                alignment_list.extend(matches[0][0])
+                del old_fasta_dict[matches[0][1]]
+                del new_fasta_dict[new_seq]
+            elif len(matches) > 1:
+                logging.warning('Failed one to one mapping: %s has %d matches: %s\n' % (new_fasta_dict[new_seq]['id'], len(matches), ','.join([x[0][0][0].split('.')[0] for x in matches])))
+
+    stages = [match_identical_sequence, match_truncated_sequence, match_split_subsequence]
+    matched_sequence_count = 0
+    for stage in range(len(stages)):
+        temp_file_name = ''
+        if debug:
+            temp_file_name = old_fasta_file + '_' + new_fasta_file + '_stage_' + str(stage + 1) + '_pickle'
+        if isfile(temp_file_name):
+            (alignment_list, old_fasta_dict, new_fasta_dict) = pickle.load(open(temp_file_name, 'rb'))
+        else:
+            if old_fasta_dict == None and new_fasta_dict == None:
+                # use sequence as dict key
+                #163023 unique sequences in original fasta file
+                #161988 unique sequences in new fasta file
+                logging.info('Reading old FASTA file (%s)...', old_fasta_file)
+                old_fasta_dict, old_fasta_count = fasta_file_to_dict(old_fasta_file, id=False, header=False, seq=True)
+                if not len(old_fasta_dict) == old_fasta_count:
+                    logging.warning('  Duplicate sequences detected in old FASTA file, %d unique sequences out of a total of %d sequences', len(old_fasta_dict), old_fasta_count)
+                    if not query_yes_no('Ignore and continue?'):
+                        sys.exit(1)
+                else:
+                    logging.info('  Unique sequences: %d', len(old_fasta_dict))
+
+                logging.info('Reading new FASTA file (%s)...', new_fasta_file)
+                new_fasta_dict, new_fasta_count = fasta_file_to_dict(new_fasta_file, id=False, header=False, seq=True)
+                if not len(new_fasta_dict) == new_fasta_count:
+                    logging.warning('Duplicate sequences detected in new FASTA file, %d unique sequences out of a total of %d sequences', len(new_fasta_dict), new_fasta_count)
+                    if not query_yes_no('Ignore and continue?'):
+                        sys.exit(1)
+                else:
+                    logging.info('  Unique sequences: %d', len(new_fasta_dict))
+
+            logging.info('Stage %d - %s:', stage + 1, stages[stage].__name__)
+            stages[stage]()
+
+            if debug:
+                pickle.dump((alignment_list, old_fasta_dict, new_fasta_dict), open(temp_file_name, 'wb'))
+
+        new_matched_sequence_count = len(set([a[0] for a in alignment_list]))
+        logging.info('  Matched sequences: %d (New : %d)', new_matched_sequence_count, new_matched_sequence_count - matched_sequence_count)
+        logging.info('  Unmatched sequences in old FASTA: %d', len(old_fasta_dict))
+        logging.info('  Unmatched sequences in new FASTA: %d', len(new_fasta_dict))
+        matched_sequence_count = new_matched_sequence_count
+        if debug:
+            fasta_dict_to_file(old_fasta_dict, old_fasta_file + '_stage_' + str(stage + 1) + '_unmatched')
+            fasta_dict_to_file(new_fasta_dict, new_fasta_file + '_stage_' + str(stage + 1) + '_unmatched')
+
+    return alignment_list, old_fasta_dict, new_fasta_dict
+
+if __name__ == '__main__':
+    import sys
+    import argparse
+    from textwrap import dedent
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=dedent("""\
+    Compares two very similar FASTA files and outputs coordinate mappings using a multi stage algorithm:
+    Stage 1: Find 100% matches
+    Stage 2: Find 100% substrings, where the full length of a new sequence can be found as a substring of a old sequence
+    Stage 3: Find cases where part of the sequence was converted into Ns
+    Outputs the 6 columns as tab-separated values: old_id, old_start, old_end, new_id, new_start, new_end
+    Originally created to compare the NCBI version to the original reference of Diaphorina citri
+    """))
+    parser.add_argument('old_fasta', type=str, help='The original FASTA file')
+    parser.add_argument('new_fasta', type=str, help='The new FASTA file')
+    parser.add_argument('-o', '--out', nargs='?', type=argparse.FileType('wb'), default=sys.stdout, help='The output alignment file (default: STDOUT)')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='If set, partial results are saved in a *_stage_i_pickle file after each stage, unmatched sequences after each stage are saved in a *_stage_i_unmatched file for both FASTA files.')
+    parser.add_argument('-hc', '--header_check', action='store_true',
+                        help='If set, confirm the detected mapping by checking the header of the new sequence for the id of the mapped old sequence.')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+
+    test_lv = 0 # debug
+    if test_lv == 1:
+        args = parser.parse_args(['diaci1.1.fa', '121845_ref_Diaci_psyllid_genome_assembly_version_1.1_chrUn.fa'])
+        alignment_list, old_fasta_dict, new_fasta_dict = fasta_diff(args.old_fasta, args.new_fasta, debug=True)
+
+        # Looks like we're done! Output alignment_list
+        pickle.dump(alignment_list, open('alignment_list.pickle', 'wb'))
+        with open('alignment_list.tsv', 'wb') as f:
+            for alignment in alignment_list:
+                f.write('\t'.join([str(a) for a in alignment]) + '\n')
+    else:
+        args = parser.parse_args()
+        alignment_list, old_fasta_dict, new_fasta_dict = fasta_diff(args.old_fasta, args.new_fasta, debug=args.debug, header_check=args.header_check)
+        if args.debug:
+            alignment_list_pickle_file = args.out.name + '_pickle'
+            if args.out.name == '<stdout>':
+                alignment_list_pickle_file = 'alignment_list_pickle'
+            pickle.dump(alignment_list, open(alignment_list_pickle_file, 'wb'))
+        for alignment in alignment_list:
+            args.out.write('\t'.join([str(a) for a in alignment]) + '\n')
+        args.out.close()
